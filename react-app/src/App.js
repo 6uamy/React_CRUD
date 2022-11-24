80ed2309d636f53b3e72f4ac873802d0bad7ca2e
Hwang Seong Taek <hst007@naver.com>
2022-11-24 18:05:30 +0900

    feat: study event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 
 function Header(props) {
   return <header>
-    <h1><a href='/'>{props.title}</a></h1>
+    <h1><a href='/' onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
 function Nav(props) {
   const lis = [];
   props.topics.map(t => 
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>));
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>));
   return <nav>
     <ol>
       {lis}
@@ -32,8 +40,12 @@ function App() {
   ];
   return (
     <div>
-      <Header title='REACT'></Header>
-      <Nav topics={topics}></Nav>
+      <Header title='REACT' onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title='Welcome' body='Hello, Web'></Article>
     </div>
   );
